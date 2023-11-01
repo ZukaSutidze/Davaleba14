10dfb15654d97cf76de98c1c0d40b24e2c4c1b06
ZukaSutidze <zuka.sutidze@gmail.com>
2023-11-02 00:53:45 +0400

    Davaleba 14

---
+++ main.js
@@ -0,0 +1,92 @@
+//task 7
+
+// let arr = [1, 3, 2]
+// let count = 0;
+
+// let arr1 = arr.sort(mysort)
+
+// function mysort(a , b){
+//     return a - b;
+// }
+
+// console.log(arr1)
+
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] === arr1[i]) {
+//         console.log("zrdadia")
+//     } else if(arr[i] != arr1[i]){
+//         console.log("ar aris zrdadia")
+//     }
+// }
+
+//task 8
+
+// let n = 36;
+
+// console.log(Math.sqrt(n))
+
+
+
+// task 9
+
+// let n = 8;
+// let count = 0;
+
+// for (let i = 0; i <= n; i++){
+//     if (n % i === 0) {
+//         count++
+//     }
+// }
+
+// console.log(count)
+
+
+
+
+//tasl 10 codewarze dawera rom sworia magram memgoni ragac areuli aqvs
+
+// const solve = (n, k) => {
+//     let monsters = 0;
+//     let score = 0;
+      
+//     for (let i = 1; ; i *= k) {
+//         score += i;
+//         if (score >= n) {
+//             monsters++;
+//             break;
+//         } else {
+//             monsters++;
+//         }
+//     }
+//     console.log(monsters)
+
+// }
+
+
+// Task 11
+
+// let input = 100;
+
+// let input2= input 
+// let cifrebi = 0;
+// let cifrtajami = 0;
+
+// while(input2 > 1 ){
+//     input2 /= 10;
+//     cifrebi++
+// }
+
+// while( input != 0) {
+//     cifrtajami += input % 10
+//     input = Math.floor(input / 10)
+// }
+
+// if (cifrebi > cifrtajami ) {
+//     console.log("ganieria")
+// } else {
+//     console.log("ara ganieria")
+// }
+
+
+
+
